96d19616397747ea971a8dbabc079da639601c3c
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2020-12-06 20:10:40 +0100

    theme example

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,6 @@ import Layout from "@components/layout/layout"
 import Title from "@components/title"
 import Head from "next/head"
 
-import styled from "styled-components"
-
-const Doo = styled.div`
-  align-items: center;
-  background-color: red;
-  color: black;
-`
-
 export const Home = (): JSX.Element => (
   <div className="container">
     <Head>
